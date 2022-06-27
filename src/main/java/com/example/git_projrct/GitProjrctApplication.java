839514210d1eb2c18637f66dce080a1a4dc92ffc
QSP <1013135308@qq.com>
2022-06-27 09:50:58 +0800

    构建项目

---
+++ src/main/java/com/example/git_projrct/GitProjrctApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_projrct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitProjrctApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitProjrctApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/git_projrct/GitProjrctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_projrct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitProjrctApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
